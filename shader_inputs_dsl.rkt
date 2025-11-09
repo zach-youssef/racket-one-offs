f76ec9e391b7652c744e5d7fb34e9a984fd6cf27
zyoussef <zacharysamiyoussef@gmail.com>
2025-11-08 20:23:55 -0500

    Add glsl

--- shader_inputs_dsl.rkt
+++ shader_inputs_dsl.rkt
@@ -41,80 +41,93 @@
                                                                     [j (in-naturals)])
                                                            (ifield j (field-type f) (field-name f))))))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (define-syntax shader-data
   (syntax-rules ()
     [(shader-data header-path shader-path data-layouts ...)
      (let ([layouts (label-layout-indices (data-layout-parse-list data-layouts ...))])
+       (display (glsl-string layouts))
        (display (cpp-string layouts)))]))
 
-(define (cpp-string layouts)
-  (define (glm-prefix type)
-    (if (ormap (λ [t] (symbol=? t type)) '(vec2 vec3 vec4 mat3 mat4)) "glm::" ""))
-  
-  (define (cpp-field field)
-    (string-append "\t" (glm-prefix (ifield-type field)) (symbol->string (ifield-type field)) " " (symbol->string (ifield-name field)) ";"))
-  
-  (define (cpp-struct buffer extras)
-    (string-append "struct " (string-titlecase (symbol->string (ibuffer-name buffer))) " {\n"
-                   (string-join (map cpp-field (ibuffer-fields buffer)) "\n")
-                   extras
-                   "\n};\n"))
-  
-  (define (cpp-ubo ubo)
-    (cpp-struct ubo ""))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-  (define (map-type-to-format type)
-    (match type
-      ['float "VK_FORMAT_R32_SFLOAT"]
-      ['uint "VK_FORMAT_R32_UINT"]
-      ['int "VK_FORMAT_R32_SINT"]
-      ['vec2 "VK_FORMAT_R32G32_SFLOAT"]
-      ['vec3 "VK_FORMAT_R32G32B32_SFLOAT"]
-      ['vec4 "VK_FORMAT_R32G32B32A32_SFLOAT"]
-      [_ (error "Unsupported vertex stream type" type)]))
-      
-  
-  (define (cpp-vert vert)
-    (cpp-struct vert
-                (let ([binding (number->string (ibuffer-index vert))]
-                      [struct-name (string-titlecase (symbol->string (ibuffer-name vert)))])
-                (string-append
-                      "\n\n\tstatic VkVertexInputBindingDescription getBindingDescription() {\n"
-                      "\t\tVkVertexInputBindingDescription bindingDescription{};\n"
-                      "\t\tbindingDescription.binding = " binding ";\n"
-                      "\t\tbindingDescription.stride = sizeof(" struct-name ");\n"
-                      "\t\t// Move to next data entry after each vertex (alternative is after each instance)\n"
-                      "\t\tbindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;\n"
-                      "\t\treturn bindingDescription;\n"
-                      "\t}\n\n"
-                      (let ([count (number->string (length (ibuffer-fields vert)))])
-                        (string-append
-                         "\tstatic std::array<VkVertexInputAttributeDescription, " count "> getAttributeDescriptions() {\n"
-                         "\t\tstd::array<VkVertexInputAttributeDescription, " count "> attributeDescriptions{};\n"))
-                      (string-join (for/list ([f (ibuffer-fields vert)])
-                                     (let ([location (number->string (ifield-index f))])
-                                       (string-append "\t\tattributeDescriptions["location"].binding = " binding ";\n"
-                                                      "\t\tattributeDescriptions["location"].location = " location ";\n"
-                                                      "\t\tattributeDescriptions["location"].format = " (map-type-to-format (ifield-type f)) ";\n"
-                                                      "\t\tattributeDescriptions["location"].offset = offsetof("struct-name", "(symbol->string (ifield-name f))");\n")))
-                                   "\n\n")
-                      "\t\treturn attributeDescriptions;\n"
-                      "\t}"))))
+(define (convert-layout header-string uniform-fn vertex-fn)
+  (λ [layouts] (string-append header-string
+                              (string-join (map (λ [l] (match l
+                                                         [(layout 'uniform-buffers ubos) (string-join (map uniform-fn ubos) "\n")]
+                                                         [(layout 'vertex-streams vert-streams) (string-join (map vertex-fn vert-streams) "\n")]
+                                                         [(layout unknown _) (error "Unknown layout type" unknown)]))
+                                                layouts) "\n"))))
 
-  
-  (define (cpp-layout l)
-    (match l
-      [(layout 'uniform-buffers ubos) (string-join (map cpp-ubo ubos) "\n")]
-      [(layout 'vertex-streams vert-streams) (string-join (map cpp-vert vert-streams) "\n")]
-      [(layout unknown _) (error "Unknown layout type" unknown)]))
-  
-  (string-append "// This code was generated and should not be modified by hand.\n"
-                 "#pragma once\n\n"
-                 "#include <vulkan/vulkan.h>\n\n"
-                 "#define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES\n"
-                 "#include <glm/glm.hpp>\n\n"
-                 (string-join (map cpp-layout layouts) "\n")))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(define (buffer-struct-name buffer)
+  (string-titlecase (symbol->string (ibuffer-name buffer))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define cpp-string (let* ([glm-prefix (λ [type] (if (ormap (λ [t] (symbol=? t type)) '(vec2 vec3 vec4 mat3 mat4)) "glm::" ""))]
+                          [cpp-field (λ [field] (string-append "\t" (glm-prefix (ifield-type field)) (symbol->string (ifield-type field)) " " (symbol->string (ifield-name field)) ";"))]
+                          [cpp-struct (λ [buffer extras](string-append "struct " (buffer-struct-name buffer) " {\n"
+                                                                       (string-join (map cpp-field (ibuffer-fields buffer)) "\n")
+                                                                       extras
+                                                                       "\n};\n"))]
+                          [map-type-to-format (λ [type](match type
+                                                         ['float "VK_FORMAT_R32_SFLOAT"]
+                                                         ['uint "VK_FORMAT_R32_UINT"]
+                                                         ['int "VK_FORMAT_R32_SINT"]
+                                                         ['vec2 "VK_FORMAT_R32G32_SFLOAT"]
+                                                         ['vec3 "VK_FORMAT_R32G32B32_SFLOAT"]
+                                                         ['vec4 "VK_FORMAT_R32G32B32A32_SFLOAT"]
+                                                         [_ (error "Unsupported vertex stream type" type)]))])
+                     (convert-layout (string-append "// This code was generated and should not be modified by hand.\n"
+                                                    "#pragma once\n\n"
+                                                    "#include <vulkan/vulkan.h>\n\n"
+                                                    "#define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES\n"
+                                                    "#include <glm/glm.hpp>\n\n")
+                                     (λ [ubo] (cpp-struct ubo ""))
+                                     (λ [vert] (cpp-struct vert
+                                                           (let ([binding (number->string (ibuffer-index vert))]
+                                                                 [struct-name (buffer-struct-name vert)])
+                                                             (string-append
+                                                              "\n\n\tstatic VkVertexInputBindingDescription getBindingDescription() {\n"
+                                                              "\t\tVkVertexInputBindingDescription bindingDescription{};\n"
+                                                              "\t\tbindingDescription.binding = " binding ";\n"
+                                                              "\t\tbindingDescription.stride = sizeof(" struct-name ");\n"
+                                                              "\t\t// Move to next data entry after each vertex (alternative is after each instance)\n"
+                                                              "\t\tbindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;\n"
+                                                              "\t\treturn bindingDescription;\n"
+                                                              "\t}\n\n"
+                                                              (let ([count (number->string (length (ibuffer-fields vert)))])
+                                                                (string-append
+                                                                 "\tstatic std::array<VkVertexInputAttributeDescription, " count "> getAttributeDescriptions() {\n"
+                                                                 "\t\tstd::array<VkVertexInputAttributeDescription, " count "> attributeDescriptions{};\n"))
+                                                              (string-join (for/list ([f (ibuffer-fields vert)])
+                                                                             (let ([location (number->string (ifield-index f))])
+                                                                               (string-append "\t\tattributeDescriptions["location"].binding = " binding ";\n"
+                                                                                              "\t\tattributeDescriptions["location"].location = " location ";\n"
+                                                                                              "\t\tattributeDescriptions["location"].format = " (map-type-to-format (ifield-type f)) ";\n"
+                                                                                              "\t\tattributeDescriptions["location"].offset = offsetof("struct-name", "(symbol->string (ifield-name f))");\n")))
+                                                                           "\n\n")
+                                                              "\t\treturn attributeDescriptions;\n"
+                                                              "\t}")))))))
+                                             
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define glsl-string (convert-layout
+                     (string-append "// This code was generated and should not be modified by hand.\n"
+                                    "#version 450\n\n")
+                     (λ [ubo] (string-append "layout(binding = "(number->string (ibuffer-index ubo))") uniform "(buffer-struct-name ubo)" {\n"
+                                             (string-join (for/list ([f (ibuffer-fields ubo)])
+                                                            (string-append "\t" (symbol->string (ifield-type f)) " " (symbol->string (ifield-name f)) ";"))
+                                                          "\n")
+                                             "\n} " (symbol->string (ibuffer-name ubo)) ";\n"))
+                     (λ [vert] (string-join (for/list ([f (ibuffer-fields vert)])
+                                              (string-append "layout(location = " (number->string (ifield-index f)) ") in " (symbol->string (ifield-type f)) " in" (string-titlecase (symbol->string (ifield-name f))) ";"))
+                                            "\n"))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (shader-data "path/to/c++/out" "path/to/glsl/out"
              (uniform-buffers (ubo0 [mat4 mvp]
                                     [float foo])
